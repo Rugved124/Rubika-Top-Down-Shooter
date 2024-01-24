a8717bc45b92536f78a0cffe298805d04147e7ea
3ternalFire <aseemrakshasbhuvankar@gmail.com>
2024-01-24 10:59:17 +0530

    Update DestroyBullet.cs

--- Assets/scripts/DestroyBullet.cs
+++ Assets/scripts/DestroyBullet.cs
@@ -6,10 +6,6 @@ public class DestroyBullet : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        //if (collision.collider.tag == "Ground" || collision.collider.tag == "Obstacle" || collision.collider.tag == "Enemies") 
-       // {
-
-       // }
         if (collision.collider.tag != null && collision.collider.tag != "Player" && collision.collider.tag != "Bullets")
         {
            Destroy(this.gameObject);
