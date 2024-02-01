2b274ce56e7e92580d5b65cad6016132b46a693f
3ternalFire <aseemrakshasbhuvankar@gmail.com>
2024-02-01 16:11:22 +0530

    Enemy Movement
    Enemy moves randomly
    Changes states if enemy is in  following range
    Follows PC when it is in range
    Changes states if player is in attacking range

--- Assets/scripts/revamped/Enemy/BaseEnemy.cs
+++ Assets/scripts/revamped/Enemy/BaseEnemy.cs
@@ -1,17 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour
 {
     public enum EnemyStates
     {
         WANDER,
-        SHIELDED,
-        SLOWED,
-        POISONED,
-        BURNED,
         CHASING,
         ABILITY,
         RUNAWAY,
@@ -38,11 +33,20 @@ public class BaseEnemy : MonoBehaviour
     Quaternion startingAngle = Quaternion.AngleAxis(-0, Vector3.up);
     Quaternion stepAngle = Quaternion.AngleAxis(5, Vector3.up);
 
+    protected Vector3 destination;
 
+    protected NavMeshAgent navMeshAgent;
+
+    protected bool isPCDetected;
+
+    public virtual void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        startingAngle = Quaternion.AngleAxis(-visionConeAngle / 2, Vector3.up);
+    }
     public virtual void  Start()
     {
-        currentEnemyStates = EnemyStates.WANDER;
-        startingAngle = Quaternion.AngleAxis(-visionConeAngle/2, Vector3.up);
+        currentEnemyStates = EnemyStates.WANDER;   
     }
 
     public virtual void Update()
@@ -67,7 +71,27 @@ public class BaseEnemy : MonoBehaviour
 
     }
 
-    public virtual void CheckForPlayer() 
+    public  void GetDestination()
+    {
+        Vector3 testPosition = transform.position + (transform.forward * 4f) + new Vector3(Random.Range(-4.5f,4.5f), 0, Random.Range(-4.5f, 4.5f));
+
+        destination = new Vector3(testPosition.x,transform.position.y,testPosition.z);
+       
+    }
+
+    public  bool NeedsDestination()
+    {
+        if (destination == Vector3.zero)
+        {
+            return true;
+        }
+        if(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            return true;
+        }
+        return false;   
+    }
+    public virtual Transform CheckForPlayer() 
     {
         
         RaycastHit hit;
@@ -87,18 +111,27 @@ public class BaseEnemy : MonoBehaviour
                 if (pc != null)
                 {
                     Debug.DrawRay(pos, direction * hit.distance, Color.red);
+                    isPCDetected = true;
+                    return pc.transform;
+                    
                 }
                 else
                 {
                     Debug.DrawRay(pos, direction * hit.distance, Color.yellow);
+                    isPCDetected = false;
+                    
                 }
             }
             else
             {
                 Debug.DrawRay(pos, direction * aggroRadius, Color.white);
+                isPCDetected = false;
+                
             }
+            
 
             direction = stepAngle * direction;
         }
+        return null;
     }
 }
--- Assets/scripts/revamped/Enemy/DrunkenShipoy.cs
+++ Assets/scripts/revamped/Enemy/DrunkenShipoy.cs
@@ -4,5 +4,81 @@ using UnityEngine;
 
 public class DrunkenShipoy : BaseEnemy
 {
+    public EnemyStates currentShipoyState;
+    Vector3 playerLocation;
+    [SerializeField]
+    private float fireBreathingRange;
 
+    Material myMaterial;
+    public override void Awake()
+    {
+        base.Awake();
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        currentShipoyState = currentEnemyStates;
+        myMaterial = GetComponent<Renderer>().material;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+
+        if (currentShipoyState != EnemyStates.ABILITY)
+        {
+            if (isPCDetected)
+            {
+                ChangeState(EnemyStates.CHASING);
+            }
+            else
+            {
+                ChangeState(EnemyStates.WANDER);
+            }
+        }
+       
+
+        switch(currentShipoyState)
+        {
+            case EnemyStates.WANDER:
+                if (NeedsDestination())
+                {
+                    GetDestination();
+                }
+                navMeshAgent.SetDestination(destination);
+                break;
+            case EnemyStates.CHASING:
+                if (CheckForPlayer() != null)
+                {
+                    playerLocation = CheckForPlayer().position;
+                }
+                navMeshAgent.SetDestination(playerLocation);
+                if(navMeshAgent.remainingDistance <= fireBreathingRange)
+                {
+                    ChangeState(EnemyStates.ABILITY);
+                }
+                break;
+
+            case EnemyStates.ABILITY:
+                Debug.Log("Ability");
+                ShipoyAttack();
+                navMeshAgent.isStopped = true;
+                break;
+        }
+    }
+    public override void ChangeState(EnemyStates state)
+    {
+        
+        if (currentShipoyState != state)
+        {
+            currentShipoyState = state;
+        }
+    }
+
+    private void ShipoyAttack()
+    {
+        myMaterial.color = Color.red;
+    }
 }
