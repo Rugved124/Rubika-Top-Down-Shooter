ca05b20181bd328338fc48dc9e4b28cc0ade7b7a
3ternalFire <aseemrakshasbhuvankar@gmail.com>
2024-03-28 16:58:25 +0530

    Flame Thrower fixed
    Now its nerfed

--- Assets/scripts/New Scripts/Bullet/FlameThrower.cs
+++ Assets/scripts/New Scripts/Bullet/FlameThrower.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class FlameThrower : BaseBullet
 {
-    public int fireDamage;
     [SerializeField]
     protected float visionConeAngle = 90f;
 
     Quaternion startingAngle = Quaternion.AngleAxis(-0, Vector3.up);
     Quaternion stepAngle = Quaternion.AngleAxis(5, Vector3.up);
 
-    [SerializeField]
     float timeBetweenTick;
 
     [SerializeField]
@@ -19,54 +17,64 @@ public class FlameThrower : BaseBullet
 
     [SerializeField]
     LayerMask obstacles;
+
+    [SerializeField]
+    private float timeBeforeFire = 1;
+
+    float chargeTime;
     public override void Start()
     {
         base.Start();
-        timeBetweenTick = 0.2f;
+        timeBetweenTick = 1/ bulletRate;
+        chargeTime = timeBeforeFire;
     }
 
     public override void Update()
     {   
-        if(!pc.isDead)
+        chargeTime -= Time.deltaTime;
+        if (!pc.isDead)
         {
-            pc.slowMultiplier = 0.3f;
+            pc.slowMultiplier = 0.5f;
             transform.forward = pc.gameObject.transform.forward;
             transform.position = pc.GetPCShoot().position;
-
-            timeBetweenAmmo -= Time.deltaTime;
-            if (timeBetweenAmmo <= 0f)
+            if (chargeTime <= 0)
             {
-                timeBetweenAmmo = 0.1f;
-                AmmoManager.instance.ReduceAmmoCount(1);
-            }
-            base.Update();
-            RaycastHit hit;
-
-            Quaternion angle = transform.rotation * startingAngle;
-
-            Vector3 direction = angle * Vector3.forward;
-
-            Vector3 pos = transform.position;
-
-            for (int i = 0; i < (visionConeAngle / 5) + 1; i++)
-            {
-                if (Physics.Raycast(pos, direction, out hit, bulletRange, obstacles))
+                timeBetweenAmmo -= Time.deltaTime;
+                if (timeBetweenAmmo <= 0f)
                 {
-                    if (hit.collider.tag == "Enemies")
+                    timeBetweenAmmo = 0.1f;
+                    AmmoManager.instance.ReduceAmmoCount(1);
+                }
+                base.Update();
+                RaycastHit hit;
+
+                Quaternion angle = transform.rotation * startingAngle;
+
+                Vector3 direction = angle * Vector3.forward;
+
+                Vector3 pos = transform.position;
+
+                for (int i = 0; i < (visionConeAngle / 5) + 1; i++)
+                {
+                    Debug.DrawRay(pos, direction.normalized * bulletRange, Color.red);
+                    if (Physics.Raycast(pos, direction, out hit, bulletRange, obstacles))
                     {
-                        timeBetweenTick -= Time.deltaTime;
-                        if (timeBetweenTick <= 0f)
+                        if (hit.collider.tag == "Enemies")
                         {
-                            timeBetweenTick = 0.2f;
-                            hit.collider.gameObject.GetComponent<Enemy>().TakeDamage(bulletDamage);
+                            timeBetweenTick -= Time.deltaTime;
+                            if (timeBetweenTick <= 0f)
+                            {
+                                timeBetweenTick = 1/bulletRate;
+                                hit.collider.gameObject.GetComponent<Enemy>().TakeDamage(bulletDamage);
+                            }
+                        }
+                        if (hit.collider.CompareTag("Shield"))
+                        {
+                            hit.collider.gameObject.GetComponent<ShieldBehaviour>().SetPoisonedForTime();
                         }
                     }
-                    if (hit.collider.CompareTag("Shield"))
-                    {
-                        hit.collider.gameObject.GetComponent<ShieldBehaviour>().SetPoisonedForTime();
-                    }
+                    direction = stepAngle * direction;
                 }
-                direction = stepAngle * direction;
             }
         }
         if (Input.GetMouseButtonUp(0) || pc.isDead || AmmoManager.instance.GetAmmoCount() <= 0)
