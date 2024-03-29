88d0f1cbbb3846dc8d103c44cde011e546a621be
3ternalFire <aseemrakshasbhuvankar@gmail.com>
2024-03-29 21:14:17 +0530

    Camera Shake script

--- Assets/scripts/New Scripts/Camera Shake/CameraShaker.cs
+++ Assets/scripts/New Scripts/Camera Shake/CameraShaker.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraShaker : MonoBehaviour
 {
     Vector3 startPos;
+    Vector3 startRotation;
     float startXRotation;
     float startYRotation;
     [SerializeField]
@@ -21,32 +22,43 @@ public class CameraShaker : MonoBehaviour
         isCameraShakeCalled = false;
         canChangeShakeDirection = true;
     }
-    public IEnumerator ShakeCamera(float easeIN, float easeOut, float duration, float magnitude, float timeBetweenEachShake)
+    public IEnumerator ShakeCamera(float duration, float magnitude, float timeBetweenEachShake, float speed)
     {
         float time = 0;
         float shakeTime = 0;
         if (!isCameraShakeCalled)
         {
-            isCameraShakeCalled=true;
+            isCameraShakeCalled = true;
             startPos = transform.position;
-            startXRotation = transform.rotation.eulerAngles.x;
-            startYRotation = transform.rotation.eulerAngles.y;
+            startRotation = transform.localEulerAngles;
         }
         while (time <= duration)
         {
             time += Time.deltaTime;
-            shakeTime += Time.deltaTime;
+            Vector3 shake = new Vector3(Random.Range(shakeAmount.x,-shakeAmount.x), Random.Range(shakeAmount.y, -shakeAmount.y), Random.Range(shakeAmount.z, -shakeAmount.z));
+            Vector3 rotation = new Vector3(Random.Range(rotationAmount.x,-rotationAmount.x), Random.Range(rotationAmount.y, -rotationAmount.y), Random.Range(rotationAmount.z, -rotationAmount.z));
             if (shakeTime >= timeBetweenEachShake)
             {
                 shakeTime = 0;
+                shake = new Vector3(Random.Range(shakeAmount.x, -shakeAmount.x), Random.Range(shakeAmount.y, -shakeAmount.y), Random.Range(shakeAmount.z, -shakeAmount.z));
+                rotation = new Vector3(Random.Range(rotationAmount.x, -rotationAmount.x), Random.Range(rotationAmount.y, -rotationAmount.y), Random.Range(rotationAmount.z, -rotationAmount.z));
             }
-            if(shakeTime < timeBetweenEachShake)
+            while(shakeTime < timeBetweenEachShake)
             {
-                 transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, transform.localEulerAngles + shakeAmount, magnitude);
-                 transform.position = Vector3.Lerp(transform.position, transform.position + shakeAmount, magnitude);
+                shakeTime += Time.deltaTime;
+                transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, transform.localEulerAngles + (rotation * magnitude), speed);
+                 transform.position = Vector3.Lerp(transform.position, transform.position + (shake * magnitude), speed);
             }
             duration -= Time.deltaTime;
             yield return null;
         }
+
+        isCameraShakeCalled = false;
+        transform.localEulerAngles = startRotation;
+    }
+
+    public bool IsCameraShaking() 
+    {
+        return isCameraShakeCalled;
     }
 }
---
+++ Assets/scripts/New Scripts/Camera Shake/VirtualCameraShake.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class VirtualCameraShake : MonoBehaviour
+{
+    private CinemachineVirtualCamera _cam;
+
+    private void Awake()
+    {
+        _cam = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public IEnumerator ShakeCamera(float shakeAmount, float duration, float speed)
+    {
+        CinemachineBasicMultiChannelPerlin cam_perlin = _cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        float time = 0;
+        while (time < duration/2) 
+        {
+            cam_perlin.m_AmplitudeGain = Mathf.Lerp(0, shakeAmount,speed);
+            duration += Time.deltaTime;
+            yield return null;
+        }
+        while (time >= duration/2 && time < duration) 
+        {
+            cam_perlin.m_AmplitudeGain = Mathf.Lerp( shakeAmount,0, speed);
+            duration += Time.deltaTime;
+            yield return null;
+        }
+        cam_perlin.m_AmplitudeGain = 0;
+    }
+
+}
